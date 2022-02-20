cb3ce27bcde2a4619b669f29768cbfbffae18d3e
khater277 <66753803+khater277@users.noreply.github.com>
2022-02-20 10:49:46 +0200

    KICK UPDATE

---
+++ lib/models/TeamModel.dart
@@ -0,0 +1,257 @@
+/// id : 64
+/// area : {"id":2072,"name":"England"}
+/// activeCompetitions : [{"id":2021,"area":{"id":2072,"name":"England"},"name":"Premier League","code":"PL","plan":"TIER_ONE","lastUpdated":"2021-04-17T02:20:14Z"},{"id":2001,"area":{"id":2077,"name":"Europe"},"name":"UEFA Champions League","code":"CL","plan":"TIER_ONE","lastUpdated":"2021-06-26T13:37:26Z"},{"id":2139,"area":{"id":2072,"name":"England"},"name":"Football League Cup","code":"FLC","plan":"TIER_THREE","lastUpdated":"2021-01-07T02:40:01Z"}]
+/// name : "Liverpool FC"
+/// shortName : "Liverpool"
+/// tla : "LIV"
+/// crestUrl : "https://crests.football-data.org/64.png"
+/// address : "Anfield Road Liverpool L4 0TH"
+/// phone : "+44 (0844) 4993000"
+/// website : "http://www.liverpoolfc.tv"
+/// email : "customercontact@liverpoolfc.tv"
+/// founded : 1892
+/// clubColors : "Red / White"
+/// venue : "Anfield"
+/// squad : [{"id":356,"name":"Thiago Alcântara","position":"Midfielder","dateOfBirth":"1991-04-11T00:00:00Z","countryOfBirth":"Italy","nationality":"Spain","shirtNumber":null,"role":"PLAYER"},{"id":1795,"name":"Alisson","position":"Goalkeeper","dateOfBirth":"1992-10-02T00:00:00Z","countryOfBirth":"Brazil","nationality":"Brazil","shirtNumber":null,"role":"PLAYER"},{"id":3233,"name":"Roberto Firmino","position":"Attacker","dateOfBirth":"1991-10-02T00:00:00Z","countryOfBirth":"Brazil","nationality":"Brazil","shirtNumber":null,"role":"PLAYER"},{"id":3320,"name":"Jordan Henderson","position":"Midfielder","dateOfBirth":"1990-06-17T00:00:00Z","countryOfBirth":"England","nationality":"England","shirtNumber":null,"role":"PLAYER"},{"id":3323,"name":"Alex Oxlade-Chamberlain","position":"Midfielder","dateOfBirth":"1993-08-15T00:00:00Z","countryOfBirth":"England","nationality":"England","shirtNumber":null,"role":"PLAYER"},{"id":3626,"name":"Sadio Mané","position":"Attacker","dateOfBirth":"1992-04-10T00:00:00Z","countryOfBirth":"Senegal","nationality":"Senegal","shirtNumber":null,"role":"PLAYER"},{"id":3663,"name":"Divock Origi","position":"Attacker","dateOfBirth":"1995-04-18T00:00:00Z","countryOfBirth":"Belgium","nationality":"Belgium","shirtNumber":null,"role":"PLAYER"},{"id":3754,"name":"Mohamed Salah","position":"Attacker","dateOfBirth":"1992-06-15T00:00:00Z","countryOfBirth":"Egypt","nationality":"Egypt","shirtNumber":null,"role":"PLAYER"},{"id":4092,"name":"Diogo Jota","position":"Attacker","dateOfBirth":"1996-12-04T00:00:00Z","countryOfBirth":"Portugal","nationality":"Portugal","shirtNumber":null,"role":"PLAYER"},{"id":7383,"name":"Kostas Tsimikas","position":"Defender","dateOfBirth":"1996-05-12T00:00:00Z","countryOfBirth":"Greece","nationality":"Greece","shirtNumber":null,"role":"PLAYER"},{"id":7856,"name":"Loris Karius","position":"Goalkeeper","dateOfBirth":"1993-06-22T00:00:00Z","countryOfBirth":"Germany","nationality":"Germany","shirtNumber":null,"role":"PLAYER"},{"id":7861,"name":"James Milner","position":"Defender","dateOfBirth":"1986-01-04T00:00:00Z","countryOfBirth":"England","nationality":"England","shirtNumber":null,"role":"PLAYER"},{"id":7862,"name":"Joe Gomez","position":"Defender","dateOfBirth":"1997-05-23T00:00:00Z","countryOfBirth":"England","nationality":"England","shirtNumber":null,"role":"PLAYER"},{"id":7865,"name":"Joël Matip","position":"Defender","dateOfBirth":"1991-08-08T00:00:00Z","countryOfBirth":"Germany","nationality":"Cameroon","shirtNumber":null,"role":"PLAYER"},{"id":7867,"name":"Trent Alexander-Arnold","position":"Defender","dateOfBirth":"1998-10-07T00:00:00Z","countryOfBirth":"England","nationality":"England","shirtNumber":null,"role":"PLAYER"},{"id":7868,"name":"Andrew Robertson","position":"Defender","dateOfBirth":"1994-03-11T00:00:00Z","countryOfBirth":"Scotland","nationality":"Scotland","shirtNumber":null,"role":"PLAYER"},{"id":7869,"name":"Virgil van Dijk","position":"Defender","dateOfBirth":"1991-07-08T00:00:00Z","countryOfBirth":"Netherlands","nationality":"Netherlands","shirtNumber":null,"role":"PLAYER"},{"id":7873,"name":"Curtis Jones","position":"Midfielder","dateOfBirth":"2001-01-30T00:00:00Z","countryOfBirth":"England","nationality":"England","shirtNumber":null,"role":"PLAYER"},{"id":8197,"name":"Adrián","position":"Goalkeeper","dateOfBirth":"1987-01-03T00:00:00Z","countryOfBirth":"Spain","nationality":"Spain","shirtNumber":null,"role":"PLAYER"},{"id":8749,"name":"Fabinho","position":"Midfielder","dateOfBirth":"1993-10-23T00:00:00Z","countryOfBirth":"Brazil","nationality":"Brazil","shirtNumber":null,"role":"PLAYER"},{"id":9542,"name":"Ibrahima Konaté","position":"Defender","dateOfBirth":"1999-05-25T00:00:00Z","countryOfBirth":"France","nationality":"France","shirtNumber":null,"role":"PLAYER"},{"id":9547,"name":"Naby Keïta","position":"Midfielder","dateOfBirth":"1995-02-10T00:00:00Z","countryOfBirth":"Guinea","nationality":"Guinea","shirtNumber":null,"role":"PLAYER"},{"id":15100,"name":"Takumi Minamino","position":"Attacker","dateOfBirth":"1995-01-16T00:00:00Z","countryOfBirth":"Japan","nationality":"Japan","shirtNumber":null,"role":"PLAYER"},{"id":22396,"name":"Luis Díaz","position":"Attacker","dateOfBirth":"1997-01-13T00:00:00Z","countryOfBirth":"Colombia","nationality":"Colombia","shirtNumber":null,"role":"PLAYER"},{"id":102046,"name":"Caoimhin Kelleher","position":"Goalkeeper","dateOfBirth":"1998-11-23T00:00:00Z","countryOfBirth":"Republic of Ireland","nationality":"Ireland","shirtNumber":null,"role":"PLAYER"},{"id":124824,"name":"Harvey Elliott","position":"Attacker","dateOfBirth":"2003-04-04T00:00:00Z","countryOfBirth":"England","nationality":"England","shirtNumber":null,"role":"PLAYER"},{"id":135636,"name":"Rhys Williams","position":"Defender","dateOfBirth":"2001-02-03T00:00:00Z","countryOfBirth":"England","nationality":"England","shirtNumber":null,"role":"PLAYER"}]
+/// lastUpdated : "2022-02-10T19:30:22Z"
+
+class TeamModel {
+  TeamModel({
+      this.id, 
+      this.area, 
+      this.activeCompetitions, 
+      this.name, 
+      this.shortName, 
+      this.tla, 
+      this.crestUrl, 
+      this.address, 
+      this.phone, 
+      this.website, 
+      this.email, 
+      this.founded, 
+      this.clubColors, 
+      this.venue, 
+      this.squad, 
+      this.lastUpdated,});
+
+  TeamModel.fromJson(dynamic json) {
+    id = json['id'];
+    area = json['area'] != null ? Area.fromJson(json['area']) : null;
+    if (json['activeCompetitions'] != null) {
+      activeCompetitions = [];
+      json['activeCompetitions'].forEach((v) {
+        activeCompetitions?.add(ActiveCompetitions.fromJson(v));
+      });
+    }
+    name = json['name'];
+    shortName = json['shortName'];
+    tla = json['tla'];
+    crestUrl = json['crestUrl'];
+    address = json['address'];
+    phone = json['phone'];
+    website = json['website'];
+    email = json['email'];
+    founded = json['founded'];
+    clubColors = json['clubColors'];
+    venue = json['venue'];
+    if (json['squad'] != null) {
+      squad = [];
+      json['squad'].forEach((v) {
+        squad?.add(Squad.fromJson(v));
+      });
+    }
+    lastUpdated = json['lastUpdated'];
+  }
+  int? id;
+  Area? area;
+  List<ActiveCompetitions>? activeCompetitions;
+  String? name;
+  String? shortName;
+  String? tla;
+  String? crestUrl;
+  String? address;
+  String? phone;
+  String? website;
+  String? email;
+  int? founded;
+  String? clubColors;
+  String? venue;
+  List<Squad>? squad;
+  String? lastUpdated;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    if (area != null) {
+      map['area'] = area?.toJson();
+    }
+    if (activeCompetitions != null) {
+      map['activeCompetitions'] = activeCompetitions?.map((v) => v.toJson()).toList();
+    }
+    map['name'] = name;
+    map['shortName'] = shortName;
+    map['tla'] = tla;
+    map['crestUrl'] = crestUrl;
+    map['address'] = address;
+    map['phone'] = phone;
+    map['website'] = website;
+    map['email'] = email;
+    map['founded'] = founded;
+    map['clubColors'] = clubColors;
+    map['venue'] = venue;
+    if (squad != null) {
+      map['squad'] = squad?.map((v) => v.toJson()).toList();
+    }
+    map['lastUpdated'] = lastUpdated;
+    return map;
+  }
+
+}
+
+/// id : 356
+/// name : "Thiago Alcântara"
+/// position : "Midfielder"
+/// dateOfBirth : "1991-04-11T00:00:00Z"
+/// countryOfBirth : "Italy"
+/// nationality : "Spain"
+/// shirtNumber : null
+/// role : "PLAYER"
+
+class Squad {
+  Squad({
+      this.id, 
+      this.name, 
+      this.position, 
+      this.dateOfBirth, 
+      this.countryOfBirth, 
+      this.nationality, 
+      this.shirtNumber, 
+      this.role,});
+
+  Squad.fromJson(dynamic json) {
+    id = json['id'];
+    name = json['name'];
+    position = json['position'];
+    dateOfBirth = json['dateOfBirth'];
+    countryOfBirth = json['countryOfBirth'];
+    nationality = json['nationality'];
+    shirtNumber = json['shirtNumber'];
+    role = json['role'];
+  }
+  int? id;
+  String? name;
+  String? position;
+  String? dateOfBirth;
+  String? countryOfBirth;
+  String? nationality;
+  dynamic shirtNumber;
+  String? role;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['name'] = name;
+    map['position'] = position;
+    map['dateOfBirth'] = dateOfBirth;
+    map['countryOfBirth'] = countryOfBirth;
+    map['nationality'] = nationality;
+    map['shirtNumber'] = shirtNumber;
+    map['role'] = role;
+    return map;
+  }
+
+}
+
+/// id : 2021
+/// area : {"id":2072,"name":"England"}
+/// name : "Premier League"
+/// code : "PL"
+/// plan : "TIER_ONE"
+/// lastUpdated : "2021-04-17T02:20:14Z"
+
+class ActiveCompetitions {
+  ActiveCompetitions({
+      this.id, 
+      this.area, 
+      this.name, 
+      this.code, 
+      this.plan, 
+      this.lastUpdated,});
+
+  ActiveCompetitions.fromJson(dynamic json) {
+    id = json['id'];
+    area = json['area'] != null ? Area.fromJson(json['area']) : null;
+    name = json['name'];
+    code = json['code'];
+    plan = json['plan'];
+    lastUpdated = json['lastUpdated'];
+  }
+  int? id;
+  Area? area;
+  String? name;
+  String? code;
+  String? plan;
+  String? lastUpdated;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    if (area != null) {
+      map['area'] = area?.toJson();
+    }
+    map['name'] = name;
+    map['code'] = code;
+    map['plan'] = plan;
+    map['lastUpdated'] = lastUpdated;
+    return map;
+  }
+
+}
+
+/// id : 2072
+/// name : "England"
+
+class Area {
+  Area({
+      this.id, 
+      this.name,});
+
+  Area.fromJson(dynamic json) {
+    id = json['id'];
+    name = json['name'];
+  }
+  int? id;
+  String? name;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['name'] = name;
+    return map;
+  }
+
+}
+
+/// id : 2072
+/// name : "England"
+
+class Area {
+  Area({
+      this.id, 
+      this.name,});
+
+  Area.fromJson(dynamic json) {
+    id = json['id'];
+    name = json['name'];
+  }
+  int? id;
+  String? name;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['name'] = name;
+    return map;
+  }
+
+}
